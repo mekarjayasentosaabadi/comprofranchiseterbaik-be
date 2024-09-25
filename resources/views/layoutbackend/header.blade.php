d5ed5a3eb44839d7364c152cdf4810adaee29133
mamanssuparman <63030102+mamanssuparman@users.noreply.github.com>
2024-09-25 21:58:22 +0700

    fixing template and keywords

--- resources/views/layoutbackend/header.blade.php
+++ resources/views/layoutbackend/header.blade.php
@@ -2,9 +2,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="PIXINVENT">
+    <meta name="description" content="Backpanel Franchiseterbaik merupakan sebuah web yang menyediakan layanan Franchise">
+    <meta name="keywords" content="Franchise terbaik, Franchiseterbaik, SamDev">
+    <meta name="author" content="SamDev">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link rel="apple-touch-icon" href="{{ asset('') }}assetsbackend/app-assets/images/ico/apple-icon-120.png">
--- resources/views/pagesbackend/loginpage.blade.php
+++ resources/views/pagesbackend/loginpage.blade.php
@@ -7,8 +7,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description" content="Backpanel Franchiseterbaik merupakan sebuah web yang menyediakan layanan Franchise">
+    <meta name="keywords" content="Franchise terbaik, Franchiseterbaik, SamDev">
     <meta name="author" content="PIXINVENT">
     <title>Login Page Franchiseterbaik</title>
     <link rel="apple-touch-icon" href="{{ asset('') }}assetsbackend/app-assets/images/ico/apple-icon-120.png">
