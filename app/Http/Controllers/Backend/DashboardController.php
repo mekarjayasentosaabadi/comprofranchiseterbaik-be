9419c10a44eae2d262ce64c4324a2c736dc63dd8
mamanssuparman <63030102+mamanssuparman@users.noreply.github.com>
2024-10-05 06:15:36 +0700

    fix(dashboard): fixing dashboard count products

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Models\User;
 use App\Models\Master;
 use App\Models\Listitem;
+use App\Models\Franchise;
 use App\Models\Mediasocial;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -14,7 +15,7 @@ class DashboardController extends Controller
     public function index(){
         $menu = "dashboard";
         $sumDataUsers = User::count();
-        $sumDataProducts = Master::count();
+        $sumDataProducts = Franchise::count();
         $sumDataListitem = Listitem::count();
         $sumDataMediasocial = Mediasocial::count();
         return view('pagesbackend.dashboard.index', compact('menu', 'sumDataUsers', 'sumDataProducts', 'sumDataListitem', 'sumDataMediasocial'));
