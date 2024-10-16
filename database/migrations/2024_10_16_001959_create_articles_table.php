38942456c28abc14aa0966b50787313c15e77e89
mamanssuparman <63030102+mamanssuparman@users.noreply.github.com>
2024-10-16 07:27:52 +0700

    feat(article): first commit migration

---
+++ database/migrations/2024_10_16_001959_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('slug');
+            $table->string('thumbnail');
+            $table->text('content');
+            $table->string('logo')->nullable();
+            $table->integer('is_publish')->comment('0: Tidak Publish, 1: Publish')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
